035328bf818873e9dddf18d7d5dd677b058a7ea3
Islam Kamilov <kamilov6@gmail.com>
2021-03-11 13:55:56 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div><h1>{{ pageTitle }}</h1>
+      <app-products></app-products>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'APM';
+export class AppComponent{
+  pageTitle: string = "Acme Product Management";
 }
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+
+@Component({
+    selector: "app-products",
+    templateUrl: "./product-list.component.html",
+    styleUrls: ["./product-list.component.css"],
+})
+export class ProductListComponent implements OnInit {
+    pageTitle: string = "Product List";
+    listFilter: string = "cart";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    products: IProduct[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2019",
+            "description": "15 gallon capacity rolling garden",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "assets/images/garden_cart.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2019",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png" 
+        }
+        
+    ];
+    ngOnInit(): void {}
+    toggleImage() : void {
+        this.showImage = !this.showImage;
+    }
+
+}
